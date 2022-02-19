d562baf4ea105eb98c6bc4b424e66be678293b4b
joakimai <joakimai@kth.se>
2022-02-19 19:16:16 +0100

    test (#4): Adds test class for the graph class.

---
+++ src/test/java/com/thealgorithms/datastructures/AdjacencyListGraphTest.java
@@ -0,0 +1,13 @@
+package com.thealgorithms.datastructures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AdjacencyListGraphTest {
+    @Test
+    void t1()
+    {
+        // 
+    }
+}
