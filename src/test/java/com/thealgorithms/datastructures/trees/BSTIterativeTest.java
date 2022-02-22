8f3d403f592841510b539f7e48ee20a0c51f3645
Lisa Balzar <lisamariebalzar@gmail.com>
2022-02-22 13:06:46 +0100

    test (#27): adds more tests to BSTIterative

--- src/test/java/com/thealgorithms/datastructures/trees/BSTIterativeTest.java
+++ src/test/java/com/thealgorithms/datastructures/trees/BSTIterativeTest.java
@@ -21,7 +21,9 @@ public class BSTIterativeTest {
     public void testPostorderPrintMessageEmptyTree() {
         BSTIterative tree = new BSTIterative();
         tree.postorder();
-        assertEquals("This BST is empty.", out.toString().trim());
+        String expected = "This BST is empty.";
+        String actual = out.toString().trim();
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -34,6 +36,36 @@ public class BSTIterativeTest {
         String expected = "40 30 9 3";
         tree.postorder();
         assertEquals("Postorder traversal of this tree is:\n" + expected, out.toString().trim());
+    }
+
+    @Test
+    public void testPreorderPrintMessageEmptyTree() {
+        BSTIterative tree = new BSTIterative();
+        tree.preorder();
+        String expected = "This BST is empty.";
+        String actual = out.toString().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPreorderPrintMessageTree(){
+        BSTIterative tree = new BSTIterative();
+        tree.add(3);
+        tree.add(9);
+        tree.add(30);
+        tree.add(40);
+        String expected = "3 9 30 40";
+        tree.preorder();
+        assertEquals("Preorder traversal of this tree is:\n" + expected, out.toString().trim());
+    }
+
+    @Test
+    public void testAddAndRemoveNode() {
+        BSTIterative tree = new BSTIterative();
+        tree.add(3);
+        assertTrue(tree.find(3));
+        tree.remove(3);
+        assertFalse(tree.find(3));
     }
 
     @AfterEach
